3612ca7de51c9219f8dedc438354736c5eec3072
PrintPractical <91225524+PrintPractical@users.noreply.github.com>
2023-09-05 19:54:02 -0400

    Added new RNG interface, standard RNG, and tests (#1)
    * Added new RNG interface, standard RNG, and tests
    
    * Removing seed test, as it changes per machine

---
+++ src/utilities/rng/StandardRng.h
@@ -0,0 +1,41 @@
+/**
+ * @file StandardRng.h
+ * @author Chris Buchter
+ * @brief Implementation of the interface using std rand.
+ * @version 0.1
+ * @date 2023-09-05
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <IRng.h>
+
+namespace CasinoSDK {
+namespace Utilities {
+/**
+ * @brief Implementation of a Standard RNG using std rand.
+ */
+class StandardRng : public IRng {
+public:
+    /**
+     * @brief Construct a new Standard Rng object.
+     */
+    StandardRng() = default;
+    /**
+     * @brief Destroy the Standard Rng object.
+     */
+    virtual ~StandardRng() = default;
+
+public:
+    /**
+     * @see IRng::Seed()
+     */
+    void Seed(unsigned int seed) override;
+    /**
+     * @see IRng::RandomInt()
+     */
+    int RandomInt(int min, int max) noexcept(false) override;
+};
+}
+}
---
+++ test/utilities/rng/StandardRng.cpp
@@ -0,0 +1,24 @@
+#include <StandardRng.h>
+#include <gtest/gtest.h>
+#include <stdexcept>
+
+using namespace CasinoSDK::Utilities;
+
+TEST(StandardRngTest, BoundsTest) {
+    auto rng = StandardRng();
+    for(int i = 0; i < 1000; i++) {
+        auto res = rng.RandomInt(1, 100);
+        EXPECT_GE(res, 1);
+        EXPECT_LE(res, 100);
+    }
+}
+
+TEST(StandardRngTest, RandomIntMaxLTMin) {
+    auto rng = StandardRng();
+    EXPECT_THROW(rng.RandomInt(10, 5), std::runtime_error);
+}
+
+TEST(StandardRngTest, RandomIntMaxEQMin) {
+    auto rng = StandardRng();
+    EXPECT_THROW(rng.RandomInt(10, 10), std::runtime_error);
+}
